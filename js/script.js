52eaf80cb57a2cf7e1ac66c278282229c1bda71f
ThubeesanKugathas <Thubeesan@outlook.com>
2022-01-13 15:44:01 +0100

    colored box by input vue.js

--- js/script.js
+++ js/script.js
@@ -2,12 +2,13 @@ new Vue({
     el: "#app",
     data: {
         message: 'Hello World',
-        image: 'https://unsplash.it/600/300?image=',
+        image: 'https://unsplash.it/300/300?image=',
         name: '',
+        color: '',
     },
     methods: {
-        saluta : function () {
+        sayHello: function () {
             alert(`Ciao ${this.name}`);
-        }
+        },
     }
 });
