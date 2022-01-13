f38088f3daace21309d5fe3b5d690473e4ae6378
ThubeesanKugathas <Thubeesan@outlook.com>
2022-01-13 14:59:55 +0100

    input with alter by method vue.js

--- js/script.js
+++ js/script.js
@@ -3,5 +3,11 @@ new Vue({
     data: {
         message: 'Hello World',
         image: 'https://unsplash.it/600/300?image=',
+        name: '',
     },
+    methods: {
+        saluta : function () {
+            alert(`Ciao ${this.name}`);
+        }
+    }
 });
